553fbc1cf735b4d25babb0caebe2467c61854ee2
Marcelo Moreira <profissionalsmbr@gmail.com>
2023-02-22 15:02:23 -0300

    att

--- resources/js/components/pages/internal/flightplans-logs/logs/modal/LogImageGeneration.js
+++ resources/js/components/pages/internal/flightplans-logs/logs/modal/LogImageGeneration.js
@@ -67,19 +67,19 @@ export const LogImageGeneration = React.memo((props) => {
                         <iframe
                             id="iframe-content"
                             onLoad={(e) => e.target.contentWindow.postMessage({ type: 'log-creation-request', log: props.actual_log }, `${process.env.MIX_APP_URL}`)}
-                        src={`${process.env.MIX_APP_URL}/internal/map-modal`}
-                        style={{ width: "100%", height: "100%" }}
+                            src={`${process.env.MIX_APP_URL}/map-modal`}
+                            style={{ width: "100%", height: "100%" }}
                         ></iframe>
-                </div>
-            </DialogContent>
-            <Divider />
-            <DialogActions>
-                <Button onClick={handleClose}>Cancelar</Button>
-                <Button onClick={handleSaveIframeImage} variant="contained" disabled={logImg == null}>
-                    Salvar
-                </Button>
-            </DialogActions>
-        </Dialog>
+                    </div>
+                </DialogContent>
+                <Divider />
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancelar</Button>
+                    <Button onClick={handleSaveIframeImage} variant="contained" disabled={logImg == null}>
+                        Salvar
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </>
     );
 });
