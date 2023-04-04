7134dfa4889033b971748e7eeee2561e6447f7a4
Marcelo Moreira <profissionalsmbr@gmail.com>
2023-04-04 01:48:04 -0300

    map att

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -155,7 +155,7 @@
 			</div>
 
 			<!-- BTN EXIT-->
-			<div id="btn-exit" class="m-5">
+			<div id="btn-exit" class="m-5" onclick="window.close()">
 				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
 					id="btn-exit-icon" stroke="currentColor" class="h-6 w-6 cursor-pointer text-white">
 					<path stroke-linecap="round" stroke-linejoin="round"
